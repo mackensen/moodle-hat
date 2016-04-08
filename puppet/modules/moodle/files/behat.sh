3b5e7fd09ab53f0cb15550a8c60639f4e7c8fc8f
Charles Fulton <fultonc@lafayette.edu>
2016-04-08 09:22:20 -0400

    Remove deprecated formatters; fixes #23

--- puppet/modules/moodle/files/behat.sh
+++ puppet/modules/moodle/files/behat.sh
@@ -4,6 +4,6 @@ BEHATPATH=/var/www/behat/`date +%Y%m%d_%H%M%S`/
 mkdir $BEHATPATH
 sudo sed -i "s|\$CFG->behat_faildump_path.*|\$CFG->behat_faildump_path='$BEHATPATH';|g" /var/www/moodle/htdocs/config.php
 
-vendor/bin/behat --format=moodle_progress,progress,html,failed \
-        --out=,$BEHATPATH/progress.txt,$BEHATPATH/status.html,$BEHATPATH/failed.txt \
+vendor/bin/behat --format=moodle_progress --out=std \
+        --format=progress --out $BEHATPATH/progress.txt \
         --config=$BEHATDATAROOT/behat/behat.yml "$@"
