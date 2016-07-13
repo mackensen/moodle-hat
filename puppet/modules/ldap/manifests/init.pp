5991f37c0cbebef5d0ef2175a98b63fe571cfd1c
Charles Fulton <fultonc@lafayette.edu>
2016-07-13 15:57:18 -0400

    Add eduperson schema; fixes #28

--- puppet/modules/ldap/manifests/init.pp
+++ puppet/modules/ldap/manifests/init.pp
@@ -6,4 +6,15 @@ class ldap {
     rootdn => 'cn=admin,dc=example,dc=com',
     rootpw => 'password',
   }
+
+  openldap::server::schema { 'eduperson':
+    ensure => present,
+    path => '/etc/ldap/schema/eduperson.schema',
+    require => File['/etc/ldap/schema/eduperson.schema'],
+  }
+
+  file { '/etc/ldap/schema/eduperson.schema':
+    source => 'puppet:///modules/ldap/eduperson.schema',
+    mode => 755,
+  }
 }
