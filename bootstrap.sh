d1b26be509525769b494c488e86024d47dd42a93
Charles Fulton <fultonc@lafayette.edu>
2017-09-28 14:20:33 -0400

    Hold puppet module versions; fixes #46

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,10 @@ apt-get -y update
 apt-get install -y git
 
 # Puppet modules
-(puppet module list |grep puppetlabs-apache) || puppet module install puppetlabs-apache
-(puppet module list |grep puppetlabs-mysql) || puppet module install puppetlabs-mysql
-(puppet module list |grep puppetlabs-postgresql) || puppet module install puppetlabs-postgresql
-(puppet module list |grep puppetlabs-vcsrepo) || puppet module install puppetlabs-vcsrepo
+(puppet module list |grep puppetlabs-apache) || puppet module install puppetlabs-apache --version=1.11.1
+(puppet module list |grep puppetlabs-mysql) || puppet module install puppetlabs-mysql --version=3.11.0
+(puppet module list |grep puppetlabs-postgresql) || puppet module install puppetlabs-postgresql --version=4.8.0
+(puppet module list |grep puppetlabs-vcsrepo) || puppet module install puppetlabs-vcsrepo --version=1.5.0
 (puppet module list |grep puppetlabs-stdlib) || puppet module install puppetlabs-stdlib
 (puppet module list |grep saz-locales) || puppet module install saz-locales
-(puppet module list |grep camptocamp-openldap) || puppet module install camptocamp-openldap
+(puppet module list |grep camptocamp-openldap) || puppet module install camptocamp-openldap --version=1.15.0
