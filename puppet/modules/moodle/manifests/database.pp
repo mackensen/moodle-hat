8de4f05b48f6a4b1fceaf95cc76877cf566ce47d
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2017-12-18 16:26:55 -0500

    Fixes #48: minor config adjustments for utf8mb4 support

--- puppet/modules/moodle/manifests/database.pp
+++ puppet/modules/moodle/manifests/database.pp
@@ -1,6 +1,6 @@
 class moodle::database {
   class { '::mysql::server':
-    override_options => { 'mysqld' => { 'innodb_file_per_table' => 1, 'innodb_file_format' => 'barracuda' } },
+    override_options => { 'mysqld' => { 'innodb_file_per_table' => 1, 'innodb_file_format' => 'barracuda', 'innodb_large_prefix' => '1', } },
     restart => true,
   }
 
@@ -10,7 +10,7 @@ class moodle::database {
     host => 'localhost',
     grant => ['all'],
     charset => 'utf8mb4',
-    collate => 'utf8_bin',
+    collate => 'utf8mb4_bin',
     before => Exec['configure_behat', 'configure_phpunit', 'install_moodle'],
   }
 
