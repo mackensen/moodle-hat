22af0f76fce98f20a559d3a24fda31e56d2fbb80
Andrew Zito <zitoa@lafayette.edu>
2018-08-20 12:19:53 -0400

    It kinda works now

--- puppet/modules/moodle/manifests/config.pp
+++ puppet/modules/moodle/manifests/config.pp
@@ -4,15 +4,20 @@ class moodle::config {
     mpm_module => false,
   }
 
+  apache::mod { 'actions': }
+  apache::mod { 'proxy': }
+  apache::mod { 'proxy_fcgi': }
+
   apache::vhost { 'local.moodle.test':
     port => '80',
     priority => 1,
     docroot => "${moodle::docroot}",
+    custom_fragment => 'ProxyPassMatch "^/(.*\.php)$" "fcgi://localhost:9001/var/www/moodle/htdocs"'
   }->
   class {'::apache::mod::prefork':
   }->
   apache::fastcgi::server { 'fpm':
-    host       => '127.0.0.1:9000',
+    host       => '127.0.0.1:9001',
     timeout    => 15,
     flush      => false,
     faux_path  => '/var/www/php.fcgi',
--- puppet/modules/phpconf/manifests/init.pp
+++ puppet/modules/phpconf/manifests/init.pp
@@ -1,39 +1,77 @@
 class phpconf {
 
-  # include '::php::cli'
+  # package { ['php7.1', 'php7.1-cli', 'php7.1-mysql', 'php7.1-pgsql', 'php7.1-curl', 'php7.1-gd', 'php7.1-xmlrpc', 'php7.1-intl', 'php7.1-xml', 'php7.1-mbstring', 'php7.1-zip', 'php7.1-ldap', 'php7.1-xdebug', 'php7.1-fpm', 'ghostscript']:
+  #   ensure => 'present',
+  #   before => [
+  #     Exec['install_moodle'],
+  #     Exec['composer'],
+  #     File['/etc/php/7.1/mods-available/custom.ini'],
+  #   ],
+  # }
+  #
+  # file {'/etc/php/7.1/mods-available/custom.ini':
+  #   ensure => 'present',
+  #   owner => root, group => root, mode => 444,
+  #   content => "always_populate_raw_post_data = -1\n",
+  #   before => Exec['enable_custom_ini'],
+  # }
 
-  # class { 'php::cli': ensure => '7.1.8' }
 
+  # class { '::php::fpm':
+  #   pools => { },
+  # }
 
-  php::params {
-    php_package_name = 'php7.1',
-    cli_package_name = 'php7.1-cli',
-
+  php::fpm::pool { 'moodlehat':
+    user => 'www-data',
+    group => 'vagrant',
+    listen_owner => 'www-data',
+    listen_group => 'www-data',
+    listen_allowed_clients => '127.0.0.1',
+    listen => '127.0.0.1:9001',
+    # before => Class['::php::globals'],
   }
 
-  php::module { 'cli': ensure => '7.1.8' }
-  php::module { 'mysql': ensure => '7.1.8' }
-  php::module { 'pgsql': ensure => '7.1.8' }
-  php::module { 'curl': ensure => '7.1.8' }
-  php::module { 'gd': ensure => '7.1.8' }
-  php::module { 'xmlrpc': ensure => '7.1.8' }
-  php::module { 'intl': ensure => '7.1.8' }
-  php::module { 'xml': ensure => '7.1.8' }
-  php::module { 'mbstring': ensure => '7.1.8' }
-  php::module { 'zip': ensure => '7.1.8' }
-  php::module { 'ldap': ensure => '7.1.8' }
-  php::module { 'xdebug': ensure => '7.1.8' }
-  php::module { 'fpm': ensure => '7.1.8' }
-  #   ]:
+  class { '::php::globals':
+    php_version => '7.1',
+    config_root => '/etc/php/7.1',
+  } ->
+  class { '::php':
+    package_prefix => 'php7.1-',
+    fpm => true,
+    fpm_service_enable => true,
+    fpm_service_ensure => 'running',
+    # fpm_service_name => 'php-fpm',
+    dev => true,
+    composer => false,
+    pear => false,
+    phpunit => false,
+    settings => {
+      'PHP/always_populate_raw_post_data' => '-1',
+      'PHP/log_errors' => 'true',
+      'PHP/error_log' => '/var/log/php-fpm-error.log',
+    },
+    extensions => {
+      'mysql'     => { },
+      'pgsql'     => { },
+      'curl'      => { },
+      'gd'        => { },
+      'xmlrpc'    => { },
+      'intl'      => { },
+      'xml'       => { },
+      'mbstring'  => { },
+      'zip'       => { },
+      'ldap'      => { },
+      'xml'       => { },
+      'xdebug'    => { },
+    },
+    before => [
+      Exec['composer'],
+      Exec['install_moodle'],
+    ]
+  }
+
+  # class { '::php::fpm':
+  #   pools => { },
   # }
 
-  # php::fpm::pool { 'moodlehat':
-  #   user => 'www-data',
-  #   group => 'vagrant',
-  #   listen_owner => 'www-data',
-  #   listen_group => 'www-data',
-  #   listen_allowed_clients => '127.0.0.1',
-  #   listen => '127.0.0.1:9000',
-  #   before => Class['::php'],
-  # }
 }
